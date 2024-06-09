4f418ce8518433261ef29a2a2219846090019f4e
cipolee <1633272721@qq.com>
2024-06-09 11:53:06 +0800

    add file_utils make_path

--- file_utils.py
+++ file_utils.py
@@ -7,20 +7,13 @@ from pathlib import Path as path
 from collections import defaultdict
 
 
-def get_json_data_from_dir(root_dir, file_name) -> Dict[str, List[Union[int, float]]]:
-    if path(file_name).suffix != '.json':
-        return {}
-    total_metrics = defaultdict(list)  # {metric_name: [values]}
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        if path(dirpath) == path(root_dir):
-            continue
-        for cur_file in filenames:
-            if str(cur_file) == str(file_name):
-                with open(path(dirpath, cur_file), 'r', encoding='utf8')as f:
-                    cur_metrics = json.load(f)
-                for k, v in cur_metrics.items():
-                    total_metrics[k].append(v)
-    return total_metrics
+def make_path(file_or_dir_path):
+    file_or_dir_path = path(file_or_dir_path)
+    if file_or_dir_path.is_file():
+        file_or_dir_path.parent.mkdir(parents=True, exist_ok=True)
+        file_or_dir_path.touch()
+    else:
+        file_or_dir_path.mkdir(parents=True, exist_ok=True)
 
 
 def dump_json(target, file_path, mode='w', indent=None):
@@ -44,3 +37,19 @@ def load_json(file_path):
         else:
             raise Exception('wrong file_path')
     return content
+
+
+def get_json_data_from_dir(root_dir, file_name) -> Dict[str, List[Union[int, float]]]:
+    if path(file_name).suffix != '.json':
+        return {}
+    total_metrics = defaultdict(list)  # {metric_name: [values]}
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        if path(dirpath) == path(root_dir):
+            continue
+        for cur_file in filenames:
+            if str(cur_file) == str(file_name):
+                with open(path(dirpath, cur_file), 'r', encoding='utf8')as f:
+                    cur_metrics = json.load(f)
+                for k, v in cur_metrics.items():
+                    total_metrics[k].append(v)
+    return total_metrics
