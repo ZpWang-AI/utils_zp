1125f0503cf31d846003eb4029895418eb722392
zpwang <zhipangwang@gmail.com>
2024-06-14 16:08:27 +0800

    modified:   src/utils_zp/script_utils.py

--- src/utils_zp/script_utils.py
+++ src/utils_zp/script_utils.py
@@ -3,8 +3,11 @@ from pathlib import Path as path
 
 def get_info_from_script_file(file, id_sep='-', desc_sep='_'):
     file_name = path(file).stem
+    script_id = file_name.split(id_sep)[0]
+    if not script_id.isnumeric():
+        script_id = '000'
     return {
-        'id': file_name.split(id_sep)[0],
+        'id': script_id,
         'desc': file_name.split(desc_sep)[-1],
     }    
     
