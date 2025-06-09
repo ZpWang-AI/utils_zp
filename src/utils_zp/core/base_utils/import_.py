a43dbf90959f409257ea30fefe6b029e6c11f9f2
zpwang <zhipangwang@gmail.com>
2025-06-09 19:16:30 -0400

    add `from collections import defaultdict`

--- src/utils_zp/core/base_utils/import_.py
+++ src/utils_zp/core/base_utils/import_.py
@@ -12,6 +12,7 @@ import shutil
 from typing import *
 from pathlib import Path as path
 from copy import deepcopy as dcopy
+from collections import defaultdict
 from traceback import format_exc, print_exc
 from builtins import print as builtin_print
 from functools import wraps
