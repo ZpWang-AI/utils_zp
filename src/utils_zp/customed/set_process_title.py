70154ea47afb2a51b50ea939a05d195c54dea979
zpwang <zhipangwang@gmail.com>
2025-12-20 15:34:14 +0800

    update setprocesstitle

--- src/utils_zp/customed/set_process_title.py
+++ src/utils_zp/customed/set_process_title.py
@@ -5,6 +5,6 @@ def set_process_title(title='zp', add_time=False):
     from setproctitle import setproctitle
     process_title = f'{title}'
     if add_time:
-        process_title += '_'+Datetime_().format_str(1)
+        process_title += ' | '+Datetime_().format_str(1)
     setproctitle(process_title)
     
