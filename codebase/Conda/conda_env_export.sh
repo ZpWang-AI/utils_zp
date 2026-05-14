bd5f448d8436dac37c7d429ebc4c388971330a0c
zp <zhipangwang@gmail.com>
2026-05-14 09:57:10 +0800

    update conda_env_export

--- codebase/Conda/conda_env_export.sh
+++ codebase/Conda/conda_env_export.sh
@@ -21,12 +21,12 @@ tar -xzf ~/tmp/conda_env.tar.gz -C _conda_path_/_new_env_name_
 
 ### pip list
 
-pip list --format=freeze > env.txt
+# delete distribute, pip, setuptools, wheel, pywin
+pip freeze --exclude-editable | grep -vE '^(distribute|pip|setuptools|wheel|pywin)' > env.txt
+
+# Ask LLM to check the list
 
 # install while skipping the failed packages
 while read -r package; do pip install "$package" || echo "Failed to install $package"; done < env.txt
-
-# delete distribute, pip, setuptools, wheel, pywin
-grep -vE '^(distribute|pip|setuptools|wheel|pywin)' env.txt > env2.txt
-pip install -r env2.txt
+# pip install -r env.txt
 
